2190763eda250a01672b6b99c247e0f5ffb0c53f
David Bradburn <david.bradburn2@gmail.com>
2025-12-01 22:07:03 +0000

    Day 1

--- Day 1/1/1_1.py
+++ Day 1/1/1_1.py
@@ -2,7 +2,7 @@
 ###### https://adventofcode.com/2024/day/1 #######################
 #################################################################
 
-file = "test.txt"
+file = "input.txt"
 
 DAY_NO = "1"
 PART = "1"
@@ -11,3 +11,38 @@ file_path_base = "Day " + DAY_NO + "/misc/"
 
 with open(file_path_base + file, "r") as fd:
 	rawInput = fd.readlines()
+
+cleanInput = [i.strip("\n") for i in rawInput]
+print(cleanInput)
+movement = [(n[0], int(n[1:])) for n in cleanInput]
+print(movement)
+startingPos = 50
+
+MAX = 99
+MIN = 0
+
+trueCount = 0
+
+curPos = startingPos
+for (dir, num) in movement:
+	if dir == "L":
+		curPos -= num
+		if curPos < MIN:
+			while (curPos < MIN):
+				curPos += 100
+
+	else:
+		assert dir == "R"
+		curPos += num
+		if curPos > MAX:
+			curPos -= 100
+			while (curPos > MAX):
+				curPos -= 100
+	print(f"{dir}{num}, {curPos}")
+
+	if (curPos == 0):
+		trueCount += 1
+
+
+
+print(trueCount)
--- Day 1/2/1_2.py
+++ Day 1/2/1_2.py
@@ -2,7 +2,7 @@
 ###### https://adventofcode.com/2024/day/1 #######################
 #################################################################
 
-file = "test.txt"
+file = "input.txt"
 
 DAY_NO = "1"
 PART = "2"
@@ -11,3 +11,37 @@ file_path_base = "Day " + DAY_NO + "/misc/"
 
 with open(file_path_base + file, "r") as fd:
 	rawInput = fd.readlines()
+
+cleanInput = [i.strip("\n") for i in rawInput]
+print(cleanInput)
+movement = [(n[0], int(n[1:])) for n in cleanInput]
+print(movement)
+startingPos = 50
+
+MAX = 99
+MIN = 0
+
+trueCount = 0
+
+curPos = startingPos
+for (dir, num) in movement:
+	if dir == "L":
+		for n in range(num):
+			curPos += 1
+			if curPos > MAX:
+				curPos = MIN
+			if (curPos == 0):
+				trueCount += 1
+	else:
+		assert dir == "R"
+		for n in range(num):
+			curPos -= 1
+			if curPos < MIN:
+				curPos = MAX
+			if (curPos == 0):
+				trueCount += 1
+
+	print(f"{dir}{num}, {curPos}, {trueCount}")
+
+
+print(trueCount)
